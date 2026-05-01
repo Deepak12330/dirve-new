423820f4c8d92718292cc735a750cf029dbc6fb7
Deepak12330 <96013003+Deepak12330@users.noreply.github.com>
2026-05-01 14:33:26 +0530

    Add files via upload

--- profile/profile.php
+++ profile/profile.php
@@ -1,17 +1,30 @@
-<!-- <?php
+<?php
 
+	require("../php/db.php");
 	session_start();
 
 	if(!empty($_SESSION['users']))
 	{	
 
 		$user = $_SESSION['users'];
+		$fetch_data = "SELECT * FROM profile_data WHERE email = '$user'";
+		$response_data = $db->query($fetch_data);
+		$data_array = $response_data->fetch_assoc();
+		$userEmail = $data_array['username'];
+		$profiledata_pic = $data_array['profile_pic'];
+		$used_storage = $data_array['used_storage'];
+		$total_storage = $data_array['total_storage'];
+		$used_percentage = ($used_storage/$total_storage)*100;
+
+
+
 	}
 	else
 	{
+		echo "session is not started";
 	}
 
-?> -->
+?>
 
 <!DOCTYPE html>
 <html>
@@ -29,11 +42,23 @@
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 
 	<title>profile</title>
+
+	<style>
+		.thumb 
+{
+	width:75px;
+	height:75px;
+
+}
+	</style>
 </head>
 <body>
 
+	
 
 	<div class="container-fluid border">
 		
@@ -70,30 +95,137 @@
 						<form id="profileForm" enctype="multipart/form-data">
     <input type="file" name="profile_pic" class="profile_pic">
 </form>
-								<i class="fa fa-user" aria-hidden="true"></i>
+								<!-- <i class="fa fa-user" aria-hidden="true"></i> -->
+								<img src="../profile_pics/<?php echo $profiledata_pic; ?>" style="width:170px; height:170px; border-radius:50%; object-fit:cover; margin-top:-20px;">
 									
 						</div>
 
 
 						</div>
-						<h1 class="ms-5">Username</h1>
-						<button class="btn btn-primary rounded shadow"> upload  files</button>
+						<h1 class="ms-5"><?php echo $userEmail ?></h1>
+						
+						<form class="p-0" id="upload_files_form">
+
+							<div class="btn btn-primary p-0 m-0 w-80"> 
+									<input type="file" name="upload_files" style="opacity:" class="upload__files">
+							</div>
+							
+						</form>
 
 						<div class="btn btn-primary mt-4 p-0">
-							<div class="used_strge rounded">0 MB</div>
+							<div class="used_strge rounded" style="width:<?php echo $used_percentage; ?>%;
+	background-color: red;
+	margin: 0;">
+
+<?php echo $used_percentage?>%
+		</div>
 						</div>
 
-						<p class="storage_info text-center">0/5mb</p>
+						<p class="storage_info text-center"><?php echo $used_storage?>MB / <?php echo $total_storage?>  MB</p>
 
 			</div>
 			<div class="col-md-9 border right">
 					
 
 					<p class="mt-4">
+
+							<div class="row">
+
 						
+						<?php
+
+							$dataEmail = $_SESSION['users'];
+							$fetchdata = "SELECT * FROM profile_data WHERE email = '$dataEmail'";
+
+							$data_query = $db->query($fetch_data);
+
+							$responseData = $data_query->fetch_assoc();
+
+							$users_id = $responseData['id'];
+
+							$newDatausers = "SELECT * FROM users_$users_id";
+							$responseData_users = $db->query($newDatausers);
+							
+							 while($final_array = $responseData_users->fetch_assoc())
+
+							 {
+							 		$file_name = $final_array['file_name'];
+							 		$file_info = pathinfo($file_name);
+							 		$f_ext = $file_info['extension'];
+							 		$pr_file_name = $file_info['basename'];
+							 		// print_r($file_ex);
+
+
+							 		echo '
+
+							 				<div class="col-md-4 d-flex align-items-center mt-2">
+							 					<div style="margin-right:10px;">';
+
+							 					 if ($f_ext == "mp4") {
+            echo "<img src='../logos/mp4.png' class='thumb'>";
+        }
+        else if ($f_ext == "mp3") {
+            echo "<img src='../logos/mp3.png' class='thumb'>";
+        }
+        else if ($f_ext == "pdf") {
+            echo "<img src='../logos/pdf.png' class='thumb'>";
+        }
+        else if ($f_ext == "docx" || $f_ext == "doc") {
+            echo "<img src='../logos/doc.png' class='thumb'>";
+        }
+        else if ($f_ext == "xlsx" || $f_ext == "xls") {
+            echo "<img src='../logos/xls.png' class='thumb'>";
+        }
+        else if ($f_ext == "pptx" || $f_ext == "ppt") {
+            echo "<img src='../logos/ppt.png' class='thumb'>";
+        }
+        else if ($f_ext == "zip") {
+            echo "<img src='../logos/zip.png' class='thumb'>";
+        }
+        else if ($f_ext == "txt") {
+            echo "<img src='../logos/txt.png' class='thumb'>";
+        }
+        else if ($f_ext == "mov") {
+            echo "<img src='../logos/mov.png' class='thumb'>";
+        }
+        else if ($f_ext == "wmv") {
+            echo "<img src='../logos/wmv.png' class='thumb'>";
+        }
+        else if ($f_ext == "jpg" || $f_ext == "jpeg" || $f_ext == "png" || $f_ext == "gif") {
+            echo "<img src='../user_".$users_id."/".$file_name."' class='thumb'>";
+        }
+
+
+							 						echo '
+							 					</div>
+
+
+							 					<div class="">
+							 					<p>'.$file_name.'</p>
+
+							 					<hr>
+							 			<div class="d-flex justify-content-around">
+			<a href="../user_'.$users_id.'/'.$file_name.'" target="_blank"><i class="fa fa-eye" aria-hidden="true"></i></a>
+			<a href="../user_'.$users_id.'/'.$file_name.'" target="_blank" download><i class="fa fa-download" aria-hidden="true"></i></a>
+			<i class="fa fa-trash" aria-hidden="true"></i>
+							 						</div>
+							 					</div>
 
 
 
+							 				</div>
+
+							 		';							 		
+							 }
+
+
+
+
+
+
+						?>
+
+</div>
 					</p>	
 
 			</div>
@@ -124,10 +256,32 @@
             } 
         });
     });
+
+
+
+    $(".upload__files").on('change',function(){
+    	var form_data = new FormData($("#upload_files_form")[0]);
+
+    	$.ajax({
+    		type: "POST",
+    		url : "../php/upload_files.php",
+    		data : form_data,
+    		contentType : false,
+    		processData : false,
+
+    		success:function(response){
+    				console.log(response);
+
+    		}
+    		
+    	})
+    })
 });
 </script>
 
 </body>
 </html>
+
+
 
 
